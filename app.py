d4a5ed4aabd558d5fe5b8cee8db7ceb8f0a3d8ca
Abdellfetah SGHIOUAR <abdelfettah@abdel-macbook.local>
2018-07-18 13:41:31 +0200

    fix app

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def hello():
 	pod_ip = os.getenv("MY_POD_IP")
 	pod_sa = os.getenv("MY_POD_SERVICE_ACCOUNT")
 
-	html = "<h3>Hello {name}!</h3> <b>Node Name: </b> {nodename}<br/><b>POD Name: </b> {podname} <br/>"
-	"<b>Namespace:</b> {namespace} <br/> <b>Pod IP:</b> {podip} <br/> <b>Pod Service Account:</b> {pod_sa} <br/>"
+	html = "<h3>Hello {name}!</h3> <b>Node Name: </b> {nodename}<br/><b>POD Name: </b> {podname} <br/>\
+	<b>Namespace:</b> {namespace} <br/> <b>Pod IP:</b> {podip} <br/> <b>Pod Service Account:</b> {pod_sa} <br/>"
 	return html.format(name=os.getenv("NAME"), nodename=node_name, podname=pod_name, namespace=namespace, podip=pod_ip,pod_sa=pod_sa)
 
 if __name__ == "__main__":
