113725c9420255f7f6f14eccdcb917da71f48ce2
Abdellfetah SGHIOUAR <abdelfettah@abdel-macbook.local>
2018-07-18 11:47:12 +0200

    Fix hostname and nodename

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def hello():
 	host_name = socket.gethostname()
 	host_ip = socket.gethostbyname(host_name)
 	node_name = os.getenv("MY_NODE_NAME")
-	html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname}<br/><b>Host IP:</b> {ip} <br/>"
-	return html.format(name=os.getenv("NAME", "world"), hostname=host_name, ip=host_ip)
+	html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname}<br/><b>Host IP:</b> {ip} <br/><b>Node Name:</b> {nodename} <br/>"
+	return html.format(name=os.getenv("NAME"), hostname=host_name, ip=host_ip, nodename=node_name)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8090)
