a2050bfe4e7134e69973a9a4d1cdf09298cf0afa
Abdellfetah SGHIOUAR <abdelfettah@abdel-macbook.local>
2018-07-18 11:37:46 +0200

    initial push

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	host_name = socket.gethostname()
+	host_ip = socket.gethostbyname(host_name)
+	node_name = os.getenv("MY_NODE_NAME")
+	html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname}<br/><b>Host IP:</b> {ip} <br/>"
+	return html.format(name=os.getenv("NAME", "world"), hostname=host_name, ip=host_ip)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8090)
